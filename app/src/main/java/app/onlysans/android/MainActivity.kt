8687791b7f8cbf19651ff10304425193d0238087
Richard Banasiak <richard@banasiak.com>
2021-02-20 14:14:23 -0600

    Initial commit

---
+++ app/src/main/java/app/onlysans/android/MainActivity.kt
@@ -0,0 +1,17 @@
+package app.onlysans.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.onlysans.android.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.main_activity)
+    if (savedInstanceState == null) {
+      supportFragmentManager.beginTransaction()
+        .replace(R.id.container, MainFragment.newInstance())
+        .commitNow()
+    }
+  }
+}
